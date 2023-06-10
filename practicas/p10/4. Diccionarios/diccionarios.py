b28e17a4bfc02d2a99b96c6d109835b063cc0b36
aparinelli <alejoparinelli@gmail.com>
2023-06-10 15:44:07 -0300

    p10 terminada

--- practicas/p10/4. Diccionarios/diccionarios.py
+++ practicas/p10/4. Diccionarios/diccionarios.py
@@ -3,15 +3,14 @@
 
 def agrupar_por_longitud(nombre_archivo: str) -> dict:
     archivo = open(nombre_archivo, "r")
-    texto = archivo.read()
+    texto: str = archivo.read()
     archivo.close()
 
-    palabras = texto.split(" ")
+    palabras: list([str]) = texto.split(" ")
 
-    longitudes_agrupadas = {}
+    longitudes_agrupadas: dict = {}
     for palabra in palabras: 
         longitud: int = len(palabra)
-
         if longitud in longitudes_agrupadas:
             longitudes_agrupadas[longitud] += 1
         else:
@@ -19,3 +18,59 @@ def agrupar_por_longitud(nombre_archivo: str) -> dict:
     
     return longitudes_agrupadas
 
+# 4.19
+def armar_diccionario_de_promedios() -> dict:
+    archivo = open("notas.csv")
+    lineas = archivo.readlines()
+    archivo.close()
+
+    notas: dict = {}
+
+    for linea in lineas:
+        LU_materia_fecha_nota: list = linea.split(',')
+        LU: str = LU_materia_fecha_nota[0]
+        nota: int = int(LU_materia_fecha_nota[-1])
+        
+        if LU in notas:
+            notas[LU].append(nota)
+        else:
+            notas[LU] = [nota]
+    
+    promedios = {}
+
+    for LU in notas.keys():
+        cantidad_de_notas: int = len(notas[LU])
+        suma_de_notas: int = 0
+        for nota in notas[LU]:
+            suma_de_notas += nota
+        
+        promedio: float = suma_de_notas / cantidad_de_notas
+
+        promedios[LU] = promedio
+    
+    return promedios
+        
+# 4.20
+def la_palabra_mas_frecuente(nombre_archivo: str) -> str:
+    archivo = open(nombre_archivo)
+    lineas = archivo.readlines()
+    archivo.close()
+
+    apariciones = {}
+
+    for linea in lineas:
+        palabras = linea.split()
+        for palabra in palabras:
+            if palabra in apariciones:
+                apariciones[palabra] += 1
+            else:
+                apariciones[palabra] = 1
+    
+    maximo: int = 0
+    palabra_mas_frecuente: str = ""
+    for palabra in apariciones.keys():
+        if apariciones[palabra] >= maximo:
+            maximo = apariciones[palabra]
+            palabra_mas_frecuente = palabra
+        
+    return palabra_mas_frecuente
--- practicas/p10/4. Diccionarios/tests_diccionarios.py
+++ practicas/p10/4. Diccionarios/tests_diccionarios.py
@@ -1,6 +1,14 @@
 from diccionarios import *
 
 print("4. Diccionarios")
-print("4.18")
+print("\n4.18")
 print('agrupar_por_longitud("4.18.txt")')
 print(agrupar_por_longitud("4.18.txt"))
+
+print("\n4.19")
+print('armar_diccionario_de_promedios()')
+print(armar_diccionario_de_promedios())
+
+print("\n4.20")
+print('la_palabra_mas_frecuente("4.18.txt")')
+print(la_palabra_mas_frecuente("4.18.txt"))
